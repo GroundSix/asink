2fe40e8cd368fabde178a4d62086dd3b41c8dce2
Harry Lawrence <harry.lawrence@groundsix.com>
2014-04-01 11:28:01 +0100

    Ran gofmt

--- asink.go
+++ asink.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"os"
+	"./vendor/jconfig"
 	"fmt"
 	"log"
-	"sync"
+	"os"
 	"os/exec"
-	"./vendor/jconfig"
+	"sync"
 )
 
 type Command struct {
-	name 		  string
-	asyncCount 	  float64
+	name          string
+	asyncCount    float64
 	relativeCount float64
-	args 		  []string
+	args          []string
 }
 
 func main() {
 	configFile := getConfigFile()
-	if (configFile != "") {
-		config  := jconfig.LoadConfig(getConfigFile())
+	if configFile != "" {
+		config := jconfig.LoadConfig(getConfigFile())
 		command := config.GetString("command")
-		counts  := config.GetArray("count")
-		args 	:= config.GetArray("args")
+		counts := config.GetArray("count")
+		args := config.GetArray("args")
 
 		setupCommand(command, counts[0].(float64), counts[1].(float64), args)
 	}
 }
 
 func getConfigFile() string {
-	if (len(os.Args) > 1) {
-		filePath  := os.Args[1]
+	if len(os.Args) > 1 {
+		filePath := os.Args[1]
 		if _, err := os.Stat(filePath); err == nil {
 			return filePath
 		}
@@ -39,22 +39,22 @@ func getConfigFile() string {
 }
 
 func setupCommand(command string, asyncCount float64, relativeCount float64, args []interface{}) {
-	commandChan   := make(chan *Command)
+	commandChan := make(chan *Command)
 	commandStruct := new(Command)
 
 	var wg sync.WaitGroup
 
-	commandStruct.name 			= command
-	commandStruct.asyncCount    = asyncCount
+	commandStruct.name = command
+	commandStruct.asyncCount = asyncCount
 	commandStruct.relativeCount = relativeCount
 
 	argsSlice := make([]string, len(args))
 
-    for i, s := range args {
-        argsSlice[i] = s.(string)
-    }
+	for i, s := range args {
+		argsSlice[i] = s.(string)
+	}
 
-    commandStruct.args = argsSlice
+	commandStruct.args = argsSlice
 
 	for i := 0; i != int(asyncCount); i++ {
 		wg.Add(1)
@@ -69,7 +69,7 @@ func setupCommand(command string, asyncCount float64, relativeCount float64, arg
 func executeCommand(command chan *Command, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	commandData := <- command
+	commandData := <-command
 
 	for c := 0; c != int(commandData.relativeCount); c++ {
 		out, err := exec.Command(commandData.name, commandData.args...).Output()
