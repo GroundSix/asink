8e2ed3d4f4bb2ecac5d9fabc4a0e2fbf7db90422
Harry Lawrence <hazbo@gmx.com>
2014-04-13 23:31:45 +0100

    Indentation

--- asink/config.go
+++ asink/config.go
@@ -29,11 +29,11 @@ import (
 * @return string file path or empty string
 */
 func GetConfigFile() string {
-if len(os.Args) > 1 {
-    filePath := os.Args[1]
-    if _, err := os.Stat(filePath); err == nil {
-        return filePath
+    if len(os.Args) > 1 {
+        filePath := os.Args[1]
+        if _, err := os.Stat(filePath); err == nil {
+            return filePath
+        }
     }
-}
-return ""
+    return ""
 }
