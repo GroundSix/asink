143b0c1d6d393f2bd9cb50dffa552e0929136157
Harry Lawrence <hazbo@gmx.com>
2014-07-15 20:41:09 +0100

    Changed top blockdocs

--- ssh.go
+++ ssh.go
@@ -1,18 +1,16 @@
-/**
- * asink v0.0.3-dev
- *
- * (c) Ground Six
- *
- * @package asink
- * @version 0.0.3-dev
- *
- * @author Harry Lawrence <http://github.com/hazbo>
- *
- * License: MIT
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
-*/
+// asink v0.0.3-dev
+//
+// (c) Ground Six
+//
+// @package asink
+// @version 0.0.3-dev
+//
+// @author Harry Lawrence <http://github.com/hazbo>
+//
+// License: MIT
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
 
 package main
 
