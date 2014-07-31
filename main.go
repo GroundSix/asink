16ba5cb497c7dc09a55f086a46e5141685b86fd6
Harry Lawrence <hazbo@gmx.com>
2014-07-31 13:22:22 +0100

    Removed if statements from startExecutionProcess for #48

--- main.go
+++ main.go
@@ -68,16 +68,12 @@ func initAsinkWithString(json string) {
 //
 // This will need reducing soon
 func startExecutionProcess(json_data *jconfig.Config) {
-    if detectExtend(json_data) == true {
-        setupExtend(json_data)
-    }
-    if detectIncludes(json_data) == true {
-        setupIncludes(json_data)
-    }
-    if detectSshRemotes(json_data) == true {
-        setupSshRemotes(json_data)
-    }
-    if detectTasks(json_data) == true {
+    setupExtend(json_data)
+    setupIncludes(json_data)
+    setupSshRemotes(json_data)
+
+    // Keep the original legacy command API
+    if len(json_data.GetStringMap("tasks")) > 0 {
         task := setupAsinkTasks(json_data)
         task.Execute()
     } else {
@@ -98,71 +94,57 @@ func setupAsinkCommand(json_data *jconfig.Config) *asink.Command {
     return command
 }
 
-// Detects if there are any tasks to be ran
-func detectTasks(json_data *jconfig.Config) bool {
-    if len(json_data.GetStringMap("tasks")) > 0 {
-        return true
-    }
-    return false
-}
-
 // If tasks are detected, they are configured here
 func setupAsinkTasks(json_data *jconfig.Config) *asink.Task {
     task       := asink.NewTask()
-    json_tasks := json_data.GetStringMap("tasks")
+    if len(json_data.GetStringMap("tasks")) > 0 {
+        json_tasks := json_data.GetStringMap("tasks")
 
-    for task_name, cmd := range json_tasks {
-        block := validateBlock(cmd.(map[string]interface{}))
-        
-        name     := block["command"].(string)
-        counts   := convertCounts(block["count"].([]interface{}))
-        args     := convertStringArray(block["args"].([]interface{}))
-        require  := block["require"].(string)
-        group    := block["group"].(string)
-        remote   := block["remote"].(string)
-        dir      := block["dir"].(string)
+        for task_name, cmd := range json_tasks {
+            block := validateBlock(cmd.(map[string]interface{}))
+            
+            name     := block["command"].(string)
+            counts   := convertCounts(block["count"].([]interface{}))
+            args     := convertStringArray(block["args"].([]interface{}))
+            require  := block["require"].(string)
+            group    := block["group"].(string)
+            remote   := block["remote"].(string)
+            dir      := block["dir"].(string)
 
-        command := createCommand(name, counts, args, dir)
+            command := createCommand(name, counts, args, dir)
 
-        command.SetManualCallback(func(name string) {
-            runInSshSession(remote, name)
-        });
-        
-        task.AddTask(task_name, command, require, group)
-        task.SetRemote(task_name, remote)
+            command.SetManualCallback(func(name string) {
+                runInSshSession(remote, name)
+            });
+            
+            task.AddTask(task_name, command, require, group)
+            task.SetRemote(task_name, remote)
+        }
     }
-
     return task
-}
-
-// Detects if there are any SSH remotes
-// that need to be setup
-func detectSshRemotes(json_data *jconfig.Config) bool {
-    if len(json_data.GetStringMap("ssh")) > 0 {
-        return true
-    }
-    return false
 }
 
 // If SSH remotes are detected, they are setup
 // here
 func setupSshRemotes(json_data *jconfig.Config) {
-    remote       := NewRemote()
-    json_remotes := json_data.GetStringMap("ssh")
+    if len(json_data.GetStringMap("ssh")) > 0 {
+        remote       := NewRemote()
+        json_remotes := json_data.GetStringMap("ssh")
 
-    for remote_name, config := range json_remotes {
-        block := config.(map[string]interface{})
-        block  = validateBlock(block)
+        for remote_name, config := range json_remotes {
+            block := config.(map[string]interface{})
+            block  = validateBlock(block)
 
-        host     := block["host"].(string)
-        port     := block["port"].(string)
-        user     := block["user"].(string)
-        password := block["password"].(string)
-        key      := block["key"].(string)
+            host     := block["host"].(string)
+            port     := block["port"].(string)
+            user     := block["user"].(string)
+            password := block["password"].(string)
+            key      := block["key"].(string)
 
-        remote.AddRemote(remote_name, host, port, user, password)
-        if (password == "") {
-            remote.AddSshKey(remote_name, key)
+            remote.AddRemote(remote_name, host, port, user, password)
+            if (password == "") {
+                remote.AddSshKey(remote_name, key)
+            }
         }
     }
 }
@@ -175,52 +157,38 @@ func runInSshSession(remote string, command string) {
     }
 }
 
-// Detects if there are any config files that
-// should be extended
-func detectExtend(json_data *jconfig.Config) bool {
-    if len(json_data.GetString("extend")) > 0 {
-        return true
-    }
-    return false
-}
-
 // Runs extended file if is is found
 func setupExtend(json_data *jconfig.Config) {
-    extend    := json_data.GetString("extend")
-    file_name := extend + ".json"
-    if _, err := os.Stat(file_name); err == nil {
-        contents_bytes, _ := ioutil.ReadFile(file_name)
-        contents := string(contents_bytes)
-        initAsinkWithString(contents)
+    if len(json_data.GetString("extend")) > 0 {
+        extend    := json_data.GetString("extend")
+        file_name := extend + ".json"
+        if _, err := os.Stat(file_name); err == nil {
+            contents_bytes, _ := ioutil.ReadFile(file_name)
+            contents := string(contents_bytes)
+            initAsinkWithString(contents)
+        }
     }
-}
-
-// Detects if there are any config files that
-// should be included
-func detectIncludes(json_data *jconfig.Config) bool {
-    if len(json_data.GetArray("include")) > 0 {
-        return true
-    }
-    return false
 }
 
 // Sets up asink to run the included tasks
 // if they are needed
 func setupIncludes(json_data *jconfig.Config) {
-    include := json_data.GetArray("include")
-    for _, value := range include {
-        file_name := value.(string) + ".json"
-        if _, err := os.Stat(file_name); err == nil {
-            contents_bytes, _ := ioutil.ReadFile(file_name)
-            contents := string(contents_bytes)
-            contents_json := jconfig.LoadConfigString(contents)
-            json_tasks := contents_json.GetStringMap("tasks")
-            for key, task := range json_tasks {
-                task_interface := task.(map[string]interface{})
-                task_interface["tag"] = value
-                json_tasks[key] = task_interface
+    if len(json_data.GetArray("include")) > 0 {
+        include := json_data.GetArray("include")
+        for _, value := range include {
+            file_name := value.(string) + ".json"
+            if _, err := os.Stat(file_name); err == nil {
+                contents_bytes, _ := ioutil.ReadFile(file_name)
+                contents := string(contents_bytes)
+                contents_json := jconfig.LoadConfigString(contents)
+                json_tasks := contents_json.GetStringMap("tasks")
+                for key, task := range json_tasks {
+                    task_interface := task.(map[string]interface{})
+                    task_interface["tag"] = value
+                    json_tasks[key] = task_interface
+                }
+                
             }
-            
         }
     }
 }
