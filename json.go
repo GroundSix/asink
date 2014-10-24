4a3e965625129f61a5d6ae3ef13541061a1697f8
Harry Lawrence <hazbo@gmx.com>
2014-10-24 09:43:18 +0100

    Code refactor on the json parsing shizz

--- json.go
+++ json.go
@@ -43,35 +43,61 @@ func (j *Json) parse(body []byte) Parser {
 // Creates and assigns tasks using the map
 // from the Json struct
 func (j *Json) assignTasks() Parser {
-	tasks 	   := j.taskMap.StringObject("tasks")
-	tasksSlice := []asink.Task{}
-	for taskName, task := range tasks {
+	t := j.taskMap.StringObject("tasks")
+	tasks := []asink.Task{}
+	for name, task := range t {
 		c := asink.NewCommand(task["command"].(string))
-
 		j.buildCommand(&c, task)
-
-		t := j.buildTask(taskName, task, &c)
-		tasksSlice = append(tasksSlice, t)
+		tasks = append(tasks, j.buildTask(name, task, &c))
 	}
-	j.tasks = tasksSlice
+	j.tasks = tasks
 	return j
 }
 
 // Builds up the asink command using the parsed
 // JSON data
 func (j *Json) buildCommand(c *asink.Command, t typed.Typed) {
-	c.AsyncCount = t.IntsOr("count", []int{1, 1})[0]
-	c.RelCount = t.IntsOr("count", []int{1, 1})[1]
-	c.Dir = t.String("dir")
-	c.Args = t.StringsOr("args", []string{})
+	j.setAsyncCount(c, t)
+	j.setRelCount(c, t)
+	j.setDir(c, t)
+	j.setArgs(c, t)
 }
 
 // Build up the asink task using the parsed
 // JSON data
-func (j *Json) buildTask(taskName string, task typed.Typed, c *asink.Command) asink.Task {
-	t := asink.NewTask(taskName, c)
-	t.Require = task.StringOr("require", "")
-	t.Group   = task.StringOr("group", "")
+func (j *Json) buildTask(name string, task typed.Typed, c *asink.Command) asink.Task {
+	t := asink.NewTask(name, c)
+	j.setRequire(&t, task)
+	j.setGroup(&t, task)
 	return t
 }
 
+// Sets the AsyncCount for the command object
+func (j *Json) setAsyncCount(c *asink.Command, t typed.Typed) {
+	c.AsyncCount = t.IntsOr("count", []int{1, 1})[0]
+}
+
+// Sets the RelCount for the command object
+func (j *Json) setRelCount(c *asink.Command, t typed.Typed) {
+	c.RelCount = t.IntsOr("count", []int{1, 1})[1]
+}
+
+// Sets the Dir for the command object
+func (j *Json) setDir(c *asink.Command, t typed.Typed) {
+	c.Dir = t.String("dir")
+}
+
+// Sets the Args for the command object
+func (j *Json) setArgs(c *asink.Command, t typed.Typed) {
+	c.Args = t.StringsOr("args", []string{})
+}
+
+// Sets the Require for the task object
+func (j *Json) setRequire(t *asink.Task, task typed.Typed) {
+	t.Require = task.StringOr("require", "")
+}
+
+// Sets the Group for the task object
+func (j *Json) setGroup(t *asink.Task, task typed.Typed) {
+	t.Group   = task.StringOr("group", "")
+}
