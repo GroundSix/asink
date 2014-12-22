84fd4eca7892348734f5cc3c75cd9354c1695f27
Harry Lawrence <hazbo@gmx.com>
2014-12-22 17:06:44 +0000

    Added some basic meta constants

---
+++ meta.go
@@ -0,0 +1,20 @@
+// asink v0.1.1-dev
+//
+// (c) Ground Six
+//
+// @package asink
+// @version 0.1.1-dev
+//
+// @author Harry Lawrence <http://github.com/hazbo>
+//
+// License: MIT
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
+package main
+
+const appName = "asink"
+const version = "v0.1.1-dev"
+const author  = "Harry Lawrence - Ground Six"
+const email   = "harry.lawrence@groundsix.com"
