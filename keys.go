af76a7ae04053eff1b5a7049da051581edd9c37c
Harry Lawrence <hazbo@gmx.com>
2014-12-24 16:48:30 +0000

    Added func comments

--- keys.go
+++ keys.go
@@ -26,12 +26,16 @@ type Keys struct {
 	public  *rsa.PublicKey
 }
 
+// Creates a new instance of keys with a given
+// path as it's only param
 func newKeys(path string) Keys {
 	k := Keys{}
 	k.path = path
 	return k
 }
 
+// Generates a public/private key pair for
+// the Keys object
 func (k *Keys) generate() {
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
@@ -41,10 +45,12 @@ func (k *Keys) generate() {
 	k.public  = &privateKey.PublicKey
 }
 
+// Writes key to a file
 func (k Keys) writeTo(filepath string) {
 
 }
 
+// Checks to see if the keys exist or not
 func (k Keys) exists() bool {
 	if _, err := os.Stat(k.path); os.IsNotExist(err) {
 		return false
