71fff67ee1b4289089447492b3b42407348351e2
Harry Lawrence <hazbo@gmx.com>
2015-01-24 11:21:52 +0000

    Stripped server and keys functionality

--- asink.go
+++ asink.go
@@ -41,90 +41,9 @@ func main() {
             Name:  "run",
             Usage: "[tasks.yml] pass through your tasks file",
             Action: func(c *cli.Context) {                
-                conn := Connection{}
-                if c.IsSet("r") == false {
-                    if err := initAsinkWithFile(os.Args[2]); err != nil {
-                        fmt.Println(err); os.Exit(1)
-                    }
-                } else {
-                    contents, err := loadTasksFile(os.Args[2])
-                    if err != nil {
-                        fmt.Println(err); os.Exit(1)
-                    }
-
-                    conn.remote = c.String("r")
-                    if c.IsSet("i") {
-                        conn.privateKeyPath = c.String("i")
-                        conn.loadPrivateKey()
-                        conn.signRequest(contents)
-                        conn.makeRequest()
-                    } else {
-                        fmt.Println("No private key specified for creating a signed request")
-                        fmt.Println("Use -i to specify the path to your key")
-                    }
+                if err := initAsinkWithFile(os.Args[2]); err != nil {
+                    fmt.Println(err); os.Exit(1)
                 }
-            },
-            Flags: []cli.Flag{
-                cli.StringFlag{
-                    Name:  "remote, r",
-                    Usage: "remote to connect to",
-                },
-                cli.StringFlag{
-                    Name:  "identity-file, i",
-                    Usage: "path to private key",
-                },
-            },
-        },
-        {
-            Name:  "server",
-            Usage: "starts up a small server listening on port 3000",
-            Action: func(c *cli.Context) {
-                s := Server{}
-                s.Port = "3000"
-                s.AuthorizedKeyPath = ""
-                if c.IsSet("a") {
-                    s.AuthorizedKeyPath = c.String("a")
-                }
-                if c.IsSet("p") {
-                    s.Port = c.String("p")
-                }
-                s.Start()
-            },
-            Flags: []cli.Flag{
-                cli.StringFlag{
-                    Name:  "port, p",
-                    Usage: "port for asink's server to listen on",
-                },
-                cli.StringFlag{
-                    Name:  "authorized-keys, a",
-                    Usage: "path to asink's authorized_keys file",
-                },
-            },
-        },
-        {
-            Name:  "keygen",
-            Usage: "generates public/private key pair",
-            Action: func(c *cli.Context) {
-                hd, err := homedir.Dir()
-                if err != nil {
-                    panic(err)
-                }
-                p := hd + "/.asink"
-                if c.IsSet("d") {
-                    p = c.String("d")
-                }
-                k := newKeys(p)
-                k.generate()
-                if k.exists() {
-
-                }
-
-            },
-            Flags: []cli.Flag{
-                cli.StringFlag{
-                    Name:  "directory, d",
-                    Usage: "path to create public/private key pair to use with asink",
-                },
             },
         },
     }
